0326c2ca4857589d3e6ca1b2a04066950dce0e3f
Pratham Prasoon <42303128+PrasoonPratham@users.noreply.github.com>
2024-01-12 03:30:06 +0530

    Revert Spam to 10 processes.

--- token_swap/spam.ts
+++ token_swap/spam.ts
@@ -1,23 +1,9 @@
 import { spawn, ChildProcess } from "child_process";
 
-const maxProcesses = 10;
-let activeProcesses = 0;
-
-const runBenchmark = (): void => {
-  // Only spawn a new process if there's room
-  if (activeProcesses < maxProcesses) {
-    activeProcesses++;
-    const process = spawn("ts-node", ["benchmark.ts"], { stdio: "inherit" });
-
-    process.on("exit", () => {
-      activeProcesses--;
-      // Try to spawn a new process after one exits
-      runBenchmark();
-    });
-  }
+const runBenchmark = (): ChildProcess => {
+  return spawn("ts-node", ["benchmark.ts"], { stdio: "inherit" });
 };
 
-// Initial population of processes
-for (let i = 0; i < maxProcesses; i++) {
+for (let i = 0; i < 10; i++) {
   runBenchmark();
 }
